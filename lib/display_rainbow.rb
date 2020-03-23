4e0d3586f65dbbc48c040de03d6d5f13a2d5484f
Louis Liu <louis.liu2013@gmail.com>
2020-03-23 00:35:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,7 @@
-display_rainbow = ["R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"]
+def display_rainbow
+  puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+end
+
+display_rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 display_rainbow
